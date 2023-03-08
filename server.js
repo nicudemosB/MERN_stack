6fcf47b0f765a1f9b972f7c57a061e1434c519b2
Billy Nicudemos <billynicudemos@Billys-MacBook-Air.local>
2023-03-07 19:33:13 -0500

    created 404 message

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3003
+
+app.use('/', express.static(path.join(__dirname, '/public')))
+
+app.use('/', require('./routes/root'))
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('hmtl')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found'})
+    } else {
+        res.type('txt').send('404 NotFound')
+    }
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
